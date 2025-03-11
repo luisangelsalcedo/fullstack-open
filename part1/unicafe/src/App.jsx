3f5a49b837ad347242eac5eeffbf65d92bb778b7
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-03-11 08:44:48 -0500

    1️⃣  Unicafe: step 02 complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,11 @@ export function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / (all || 1);
+  const positive = (100 / (all || 1)) * good;
+
   return (
     <>
       <h1>five feedback</h1>
@@ -17,9 +22,10 @@ export function App() {
         good {good} <br />
         neutral {neutral} <br />
         bad {bad} <br />
+        all {all} <br />
+        average {average} <br />
+        positive {positive}% <br />
       </p>
     </>
   );
 }
-
-export default App;
