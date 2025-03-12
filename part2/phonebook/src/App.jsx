ab4188e71f224f33356b3db5480ee1fc418c9909
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-03-12 18:55:35 -0500

    2️⃣  The phonebook: step 3 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,55 @@
 import { useState } from "react";
 
 export function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-123456" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   const addNewName = (e) => {
     e.preventDefault();
     // validations
-    if (!newName || newName.length < 2) return;
-    if (checkRepeatedName(newName)) return;
+    if (checkIsValidName(newName) || checkIsValidNumber(newPhone)) return;
 
-    const person = { name: newName };
+    const person = { name: newName, phone: newPhone };
     setPersons((state) => [...state, person]);
+
+    // clear
     setNewName("");
+    setNewPhone("");
   };
 
-  const checkRepeatedName = (name) => {
-    if (persons.some((person) => person.name === name)) {
-      alert(`${name} is already added to phonebook`);
+  const checkIsValidNumber = (value) => {
+    // eslint-disable-next-line no-useless-escape
+    const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+
+    if (!newPhone) {
+      alert(`The phone number is required`);
+      return true;
+    }
+    if (newPhone.length < 10) {
+      alert(`The phone number must have a min. of 10 characters`);
+      return true;
+    }
+    if (!regex.test(value)) {
+      alert(`${value} isn't a valid phone number`);
+      return true;
+    }
+    return false;
+  };
+
+  const checkIsValidName = (value) => {
+    if (!newName) {
+      alert(`The name is required`);
+      return true;
+    }
+    if (newName.length < 2) {
+      alert(`The name must have a min. of 2 characters`);
+      return true;
+    }
+    if (persons.some((person) => person.name === value)) {
+      alert(`${value} is already added to phonebook`);
       return true;
     }
     return false;
@@ -28,8 +60,15 @@ export function App() {
       <h2>Phonebook</h2>
       <form onSubmit={addNewName}>
         <div>
-          name:{" "}
+          name:
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number:
+          <input
+            value={newPhone}
+            onChange={(e) => setNewPhone(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -37,8 +76,10 @@ export function App() {
       </form>
       <h2>Numbers</h2>
       <ol>
-        {persons.map(({ name }) => (
-          <li key={name}>{name}</li>
+        {persons.map(({ name, phone }) => (
+          <li key={name}>
+            🧑 {name} 📞 {phone}
+          </li>
         ))}
       </ol>
     </div>
