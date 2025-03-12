b4925d67f89085831fda95da82d5634e2576a832
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-03-12 09:02:56 -0500

    2️⃣  Courseinfo: step 08 complete

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,5 +1,5 @@
 export function Total({ parts }) {
-  const total = parts.reduce((a, el) => a + el.exercises, 0);
+  const total = parts.reduce((s, p) => s + p.exercises, 0);
 
   return <p>{`total of ${total} exercises`}</p>;
 }
