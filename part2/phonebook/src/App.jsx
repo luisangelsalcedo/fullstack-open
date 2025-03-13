5acc2564488d3c4f50eac2a7254adbf995cef766
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-03-12 19:17:02 -0500

    2️⃣  The phonebook: step 4 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 
 export function App() {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [query, setQuery] = useState("");
 
   const addNewName = (e) => {
     e.preventDefault();
     // validations
     if (checkIsValidName(newName) || checkIsValidNumber(newPhone)) return;
 
-    const person = { name: newName, phone: newPhone };
+    const person = { name: newName, number: newPhone };
     setPersons((state) => [...state, person]);
 
     // clear
@@ -55,9 +59,22 @@ export function App() {
     return false;
   };
 
+  const personsToShow = !query
+    ? persons
+    : persons.filter(({ name }) =>
+        name.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter show with:
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <h2>add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name:
@@ -76,9 +93,9 @@ export function App() {
       </form>
       <h2>Numbers</h2>
       <ol>
-        {persons.map(({ name, phone }) => (
+        {personsToShow.map(({ name, number }) => (
           <li key={name}>
-            🧑 {name} 📞 {phone}
+            {name} 📞 {number}
           </li>
         ))}
       </ol>
